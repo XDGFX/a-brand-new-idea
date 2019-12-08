df50f8a19d2a7f9c82a8ad22446e1f97f96c6291
Callum <callum.morrison@mac.com>
2019-12-08 17:59:07 +0000

    Working version

--- js/randomise.js
+++ js/randomise.js
@@ -1,13 +1,32 @@
 function randomise() {
 
+// Category
 var catNumber = Math.floor(Math.random() * (Object.keys(categories[0]).length));
 var catName = Object.keys(categories[0])[catNumber];
 
-var subcatNumber = Math.floor(Math.random() * (Object.keys(categories[0][catName]).length));
-var subcatName = categories[0][catName][subcatNumber];
+// Subcatagory
+var subcatName = categories[0][catName][Math.floor(Math.random() * (Object.keys(categories[0][catName]).length))];
 
-document.getElementById("category").innerHTML = catName.toLowerCase()
-document.getElementById("subcategory").innerHTML = subcatName.toLowerCase()
+// Aesthetic
+var aesName = aesthetics[Math.floor(Math.random() * (Object.keys(aesthetics).length))];
 
+// Cost
+var costName = cost[Math.floor(Math.random() * (Object.keys(cost).length))];
+
+// Consumer
+var consName = consumer[Math.floor(Math.random() * (Object.keys(consumer).length))];
+
+// Materials
+var matName = material[Math.floor(Math.random() * (Object.keys(material).length))];
+
+ide = document.getElementById("idea")
+
+ide.innerHTML = ide.innerHTML
++ "design <b>" + subcatName.toLowerCase() + "</b> (" + catName.toLowerCase() + "), "
++ " with a <b>" + aesName.toLowerCase() + "</b> aesthetic, "
++ "<b>" + costName.toLowerCase() + "</b> cost, "
++ " where the primary user is <b>" + consName.toLowerCase() + "</b>, "
++ " and the main material used is <b>" + matName.toLowerCase() + "</b>."
++ "<br class=\"is-size-4\">"
 
 }
